ca0f306960b54ac53ccef5f305c3fbd155c4c8b1
120percent <45733175+120percent@users.noreply.github.com>
2019-06-05 08:21:36 +0200

    Kommentiert Thomas

--- Obstacles.java
+++ Obstacles.java
@@ -4,13 +4,16 @@ import java.util.Random;
 
 public class Obstacles
 {
-    private ArrayList<Obstacle> obstacles;
+    private ArrayList<Obstacle> obstacles; 
     private ArrayList<Obstacle> removeObstacles;
     private int state;
     private static final int STILL = 1, RUN = 2;
     private int speed;
     private Random random;
-
+    /**
+     * Konstruktor
+     * Erstellt die verschiedenen Attribute und setzt teilweise Werte fest
+     */
     public Obstacles(){
         obstacles = new ArrayList<Obstacle>();
         removeObstacles = new ArrayList<Obstacle>();
@@ -20,13 +23,19 @@ public class Obstacles
         initializeObstacles();
         
     }
-
+    
+    /**
+     * @param g Grafikobjekt zum zeichnen
+     * Zeichnet ein Grafikobjekt
+     */
     public void create(Graphics g){
         for (Obstacle o : obstacles){
             o.create(g);
         }
     }
-
+    /**
+     * Setzt alles auf die Ausgangsstellung zurueck 
+     */
     public void reset(){
         state = 2;
         speed = 30;
@@ -38,7 +47,9 @@ public class Obstacles
         }
 
     }
-
+    /**
+     *  Aktualisiert die Obstacles je nach State, in welchem sie sich befinden
+     */
     public void update(){
         if(state == RUN){
             if(obstacles.size() != 0){
@@ -59,19 +70,31 @@ public class Obstacles
             }
         }
     }
-
+    /**
+     * @return obstacles
+     * Gibt ein obstacles-Objekt zurueck 
+     */
     public ArrayList<Obstacle> getObstacles(){
         return obstacles;
     }
-
+    
+    /**
+     * Setzt den Status auf STILL (Pause)
+     */
     public void stop(){
         state = STILL;
     }
-
+    
+    /**
+     * Setzt den Status auf RUN (Weiter)
+     */
     public void start(){
         state = RUN;
     }
-
+    
+    /**
+     * Hinzufügen eines <Obstacle>-Objekts; Zufällige Wahl zwischen Bird und Cactus
+     */
     public void initializeObstacles(){
         if(random.nextInt(2) == 1){
             if(random.nextInt(2) == 1){
@@ -81,7 +104,10 @@ public class Obstacles
             }
         }
     }
-
+    /**
+     * @param s neuer Geschwindigkeitswert
+     * Verändert die Geschwindigkeit der Objekte
+     */
     public void changeSpeed(int s){
         if (speed >70){
             speed = 71;
